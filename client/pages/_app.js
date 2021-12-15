3019785a5e32a59dc719b83ca869557ffe266c8b
Richard Barnes <rbarnes2@hotmail.co.uk>
2021-12-15 22:10:33 +0000

    fetch request now calling graphql inside login page due to being unable to use hooks inside NextAuth

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,11 +1,24 @@
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
+const client = new ApolloClient({
+  uri: "http//localhost:4000",
+  cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <SessionProvider>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <ApolloProvider client={client}>
+      <SessionProvider>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </ApolloProvider>
   );
 }
 
--- client/pages/api/auth/[...nextauth].js
+++ client/pages/api/auth/[...nextauth].js
@@ -1,5 +1,17 @@
 import NextAuth from "next-auth";
 import CredentialProvider from "next-auth/providers/credentials";
+import { gql, useMutation } from "@apollo/client";
+
+const LOGIN = gql`
+  mutation LoginUser($loginInput: LoginUserInput) {
+    loginUser(loginInput: $loginInput) {
+      _id
+      accessToken
+    }
+  }
+`;
+
+// const [loginUser, { data, loading, error }] = useMutation(LOGIN);
 
 export default NextAuth({
   providers: [
@@ -13,26 +25,42 @@ export default NextAuth({
         },
         password: { label: "Password", type: "password" },
       },
-      authorize: (credentials) => {
+      authorize: async (credentials) => {
         //database look up
-
-        if (
-          credentials.email === "test@test.com" &&
-          credentials.password === "123"
-        ) {
-          console.log("success!", credentials);
-          return { id: 2, name: "John", email: "johndoe@test.com" };
+        const userInput = {
+          email: credentials.email,
+          password: credentials.password,
+        };
+        const response = await fetch("http://localhost:4000/", {
+          method: "POST",
+          credentials: "include",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            query: `mutation loginUser($loginInput: LoginUserInput) {
+                loginUser(loginInput:$loginInput) {
+                  _id
+                  accessToken
+                }
+              }`,
+            variables: {
+              loginInput: userInput,
+            },
+          }),
+        });
+        const responseData = await response.json();
+        if (responseData.data) {
+          return responseData.data.loginUser;
+        } else {
+          return null;
         }
-        //login failed
-        console.log(credentials);
-        return null;
       },
     }),
   ],
   callbacks: {
     jwt: async ({ token, user }) => {
+      console.log("user inside jwt callback", user);
       if (user) {
-        token.id = user.id;
+        token.id = user.accessToken;
       }
       return token;
     },
